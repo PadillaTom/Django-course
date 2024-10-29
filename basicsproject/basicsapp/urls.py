1f332a765cd7f02f45b9d75a87b8bce04f312c1f
Tomas Padilla <padillatomasagustin@gmail.com>
2024-10-29 13:02:21 +0100

    course - Created few views with Params

--- basicsproject/basicsapp/urls.py
+++ basicsproject/basicsapp/urls.py
@@ -1,6 +1,11 @@
+from tkinter.font import names
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home", views.Home,  name="Home")
+    path("", views.Home,  name="Home"),
+    path("yahoo", views.ShowMoreMessage,  name="Yahoo"),
+    path("useVars", views.UseVariableAsResponse, name="Variables"),
+    path("getTest", views.GetRequestVars, name = "get")
 ]
--- basicsproject/basicsapp/views.py
+++ basicsproject/basicsapp/views.py
@@ -9,3 +9,21 @@ from django.http import HttpResponse
 # =====
 def Home(request):
     return HttpResponse("<h1>Hello World!!</h1>")
+
+def ShowMoreMessage(request):
+    return HttpResponse("" +
+                        "<h1>Hello World!!</h1>" +
+                        "<h2>Yahooooo!!</h2>" +
+                        "")
+
+def UseVariableAsResponse(request):
+    Message = "<h1>Welcome</h1>"
+    return HttpResponse(Message)
+
+def GetRequestVars(request):
+    if(request.method == "GET"):
+        if(request.GET.get("Message")):
+            Message = request.GET.get("Message")
+        else:
+            Message = "<h1>Should be GET</h1>"
+    return HttpResponse(Message)
