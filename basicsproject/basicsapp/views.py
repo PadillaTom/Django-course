8ab052252ff3580c2ac2e83e0363afeff4f9c00c
Tomas Padilla <padillatomasagustin@gmail.com>
2024-10-29 08:52:39 +0100

    course - Create View - AppUrls - Project Settings and Urls

--- basicsproject/basicsapp/views.py
+++ basicsproject/basicsapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+# =====
+# Function based Views
+# =====
+def Home(request):
+    return HttpResponse("<h1>Hello World!!</h1>")
